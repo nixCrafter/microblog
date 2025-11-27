b4317ed80f039c6c8213d7333d4db5b058f18e2d
nixCrafter <nixcrafter.dev@gmail.com>
2025-11-27 09:06:27 +0700

    I18n and L10n (v0.13)

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
 from config import Config
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +23,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -45,5 +53,6 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config:
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['nixcrafter.dev@gmail.com']
     POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'vi']
